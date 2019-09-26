12460e536e5073f23d3fbf5c3683d42b4e395cb9
Aditya1001001 <luciferisanangel@gmail.com>
2019-09-26 13:48:28 +0530

    vectorized implementation of gradient descent

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -17,6 +17,8 @@ for iter = 1:num_iters
     %       of the cost function (computeCost) and gradient here.
     %
         k = 1:m;
+
+        ##theta = theta - (alpha/m) * X' *((X*theta)-y);
     t1 = sum((theta(1) + theta(2) .* X(k,2)) - y(k)); % Un-Vectorized
     t2 = sum(((theta(1) + theta(2) .* X(k,2)) - y(k)) .* X(k,2)); % Un-Vectorized
     
